51010cc476b68df75647c07cdb34852d7b410e50
Luminocents <Jorda25365@live.ytech.edu>
2024-01-24 14:30:38 -0500

    Two moving pages
    In calc.html

--- calc.js
+++ calc.js
@@ -1,9 +1,21 @@
-let clicked = false;
-    document.getElementById("buttonOff").addEventListener("click", function () {
-        clicked = !clicked;
-        if (clicked) {
-        document.getElementById("buttonOff").id = "buttonOn"
+let dmg = false;
+
+document.getElementById("dmgOff").addEventListener("click", function () {
+    dmg = !dmg;
+    if (dmg) {
+        document.getElementById("dmgOff").id = "dmgOn"
     } else {
-        document.getElementById("buttonOn").id = "buttonOff"
-    } 
-    });
+        document.getElementById("dmgOn").id = "dmgOff"
+    }
+});
+
+let scatha = false;
+
+document.getElementById("scathaOff").addEventListener("click", function () {
+    scatha = !scatha;
+    if (scatha) {
+        document.getElementById("scathaOff").id = "scathaOn"
+    } else {
+        document.getElementById("scathaOn").id = "scathaOff"
+    }
+});
