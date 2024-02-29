7801c40bd1664f7298f09308ba59ca916d37cdc4
Luminocents <Jorda25365@live.ytech.edu>
2024-02-29 14:53:21 -0500

    Stuff idk dmfjsdnfehribs

---
+++ learnCode/index.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById('startButton').addEventListener('click', function () {
+    document.getElementById('startMenu').style.display = 'none';
+    document.getElementById('startMenu').style.visibility = 'hidden';
+    canvas.style.visibility = 'visible';
+});
+    
+    // read keys being pressed and released
+    document.addEventListener('keydown', handleKeydown);
+    document.addEventListener('keyup', handleKeyup);
+
+    // handle key down events
+    function handleKeydown(event) {
+        const key = event.key;
+
+    }
+
+    // handle key up events
+    function handleKeyup(event) {
+        const key = event.key;
+
+    }
