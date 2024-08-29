378c454ddf2f795a1125119646f80c734121bbc8
Luminocents <Jorda25365@live.ytech.edu>
2024-08-29 00:01:48 -0400

    A lot
    sound mainly (it's worse but I did something :smiley: )

--- PianoProject/pianoKey.gd
+++ PianoProject/pianoKey.gd
@@ -1,35 +1,46 @@
 extends Node2D
 
+var music = AudioStreamPlayer2D.new()
+var timer = Timer.new()
 var fading = false
 var strang
-var keySound
+var noteDuration
 
 func _ready() -> void:
 	strang = str(int($AnimatedSprite2D.get_parent().name.get_slice("key", 1)) + 1)
-	keySound = load("pianoKeySounds/" + strang + ".mp3")
-	$keySound.stream = keySound
+	var stream = load("pianoKeySounds/" + strang + ".mp3")
+	music.set_stream(stream)
+	music.max_polyphony = 2
+	$AnimatedSprite2D.get_parent().add_child(music)
+	$AnimatedSprite2D.get_parent().add_child(timer)
+	timer.timeout.connect(_on_timer_timeout)
 
 func _process(delta: float) -> void:
-	if fading:
-		$keySound.volume_db += -60*delta
-		
-		if $keySound.volume_db <= -60:
-			fading = false
+	pass
+	#if fading:
+		#music.volume_db += -.01 * noteDuration - .01
+		#
+		#if music.volume_db <= -80:
+			#fading = false
 
 
-func key_on(duration):
+func key_on(duration, velocity):
 	$AnimatedSprite2D.animation = "on"
-	$keySound.play()
-	$keyTimer.start(duration)
+	music.volume_db = -15 + (15 * velocity)
+	noteDuration = duration
+	music.play()
+	timer.start(duration)
+	
 
-func _on_key_timer_timeout() -> void:
+func _on_timer_timeout() -> void:
 	$AnimatedSprite2D.animation = "off"
 	fading = true
 
 func key_click():
 	$AnimatedSprite2D.animation = "on"
-	$keySound.volume_db = 0
-	$keySound.play()
+	music.volume_db = 0
+	print(strang)
+	music.play()
 	
 func key_clickOut():
 	$AnimatedSprite2D.animation = "off"
